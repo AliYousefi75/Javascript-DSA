053376e24fdbfad4d556d7657ac93d7a1e434d82
ali <ali.yousefi7518@gmail.com>
2026-08-06 13:34:56 +0430

    add dynamic array operations

--- 01-arrays/arrays.js
+++ 01-arrays/arrays.js
@@ -28,3 +28,64 @@
 
 
 //تمرین: یک آرایه داینامیک پیاده سازی کن که قادر به درج حذف یافتن و چاپ عناصر باشه
+class DinamicArray {
+    constructor(){
+        this.count =0;
+        this.items = new Array(4);
+    }
+
+    print(){
+        for(let i = 0; i < this.count; i++){
+            console.log(this.items[i])
+        }
+    }
+
+    indexOf(item){
+        for(let i = 0; i < this.count; i++){
+            if(this.items[i] == item){
+                return i;
+            }
+        }
+        return -1
+    }
+
+    insert(item){
+        if(this.items.length === this.count){
+            let newArray = new Array(this.count*2);
+            for(let i = 0; i < this.count; i++){
+                newArray[i] = this.items[i]
+            }
+            this.items = newArray;
+        }
+        this.items[this.count] = item;
+        this.count++;
+    }
+
+    removeAt(index){
+        if(index < 0 || index >= this.count){
+            return `your index is not true`
+        }
+        for(let i = index; i < this.count-1; i++){
+            this.items[i] = this.items[i+1];
+        }
+        this.count--;
+        this.items[this.count] = undefined;
+    }
+}
+
+let arr = new DinamicArray();
+
+arr.insert(10);
+arr.insert(20);
+arr.insert(30);
+arr.insert(40);
+
+arr.print();
+
+arr.insert(50);
+arr.print();
+
+console.log(arr.indexOf(30));
+
+arr.removeAt(2);
+arr.print()
