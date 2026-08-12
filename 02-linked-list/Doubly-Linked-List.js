a3fd34884c725c7c08738838b6a29bc49b567fe4
ali <ali.yousefi7518@gmail.com>
2026-08-12 13:14:23 +0430

    DoublyLinkedList-addLast

--- 02-linked-list/Doubly-Linked-List.js
+++ 02-linked-list/Doubly-Linked-List.js
@@ -12,6 +12,23 @@ class DoublyLinkedList{
         this.tail = null;
         this.length = 0;
     }
+    
+        addLast(value){
+        let newNode = new Node(value)
+
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode
+        }
+        this.length++;
+    }
+
+    
+
 
 
 }
