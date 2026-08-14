7d47c99dfc144ec082ab9f3c6b4ac4fa98b86ee4
ali <ali.yousefi7518@gmail.com>
2026-08-14 16:27:23 +0430

    refactor: modularize balanced expression validator with helper functions

--- 03-stack/expression.js
+++ 03-stack/expression.js
@@ -1,40 +1,91 @@
 import Stack from "./stack.js";
 
-function isBalanced(expression){
+// function isBalanced(expression){
 
-    const stack  = new Stack();
+//     const stack  = new Stack();
 
-    // تعریف جفت‌های باز و بسته
+//     // تعریف جفت‌های باز و بسته
+//     const pairs = {
+//         '(':')',
+//         '{':'}',
+//         '[':']',
+//         '<':'>',
+//     };
+//     // تعیین اینکه کاراکتر باز هست یا بسته
+//     const openers = Object.keys(pairs);
+//     const closers = Object.values(pairs);
+
+//     for(let char of expression){
+//         if(openers.includes(char)){
+//             // اگر علامت باز بود → بذار تو استک
+//             stack.push(char);
+//         }else if(closers.includes(char)){
+//             if(stack.isEmpty()){
+//                 return false; // استک خالی → علامت بسته‌ی اضافی
+//             }
+
+//             const lastOpener = stack.pop();
+//             if(pairs[lastOpener] !== char){
+//                 return false;// جفتشون یکی نیست
+//             }
+//         }
+//     }
+//     // بعد از بررسی همه‌ی کاراکترها، استک باید خالی باشه
+//     return stack.isEmpty();
+// }
+
+// console.log('expression');
+// console.log(isBalanced("{[()]}"));        // true
+// console.log(isBalanced("({[]})"));        // true
+// console.log(isBalanced("({[})"));         // false
+
+
+
+
+// ==============Refactore===============//
+
+function isOpener(char){
+    const openers = ['(','[','{','<'];
+    return openers.includes(char);
+}
+
+function isCloser(char){
+    const closer = [')',']','}','>'];
+    return closer.includes(char);
+}
+
+function isMatching(openers,closer){
     const pairs = {
-        '(':')',
-        '{':'}',
-        '[':']',
-        '<':'>',
+        '(': ')',
+        '[': ']',
+        '{': '}',
+        '<': '>',
     };
-    // تعیین اینکه کاراکتر باز هست یا بسته
-    const openers = Object.keys(pairs);
-    const closers = Object.values(pairs);
+    return pairs[openers] === closer;
+}
+
+function isBalanced(expression){
+    const stack = new Stack();
 
     for(let char of expression){
-        if(openers.includes(char)){
-            // اگر علامت باز بود → بذار تو استک
+        if(isOpener(char)){
             stack.push(char);
-        }else if(closers.includes(char)){
+        }else if(isCloser(char)){
             if(stack.isEmpty()){
-                return false; // استک خالی → علامت بسته‌ی اضافی
+                return false;
             }
 
             const lastOpener = stack.pop();
-            if(pairs[lastOpener] !== char){
-                return false;// جفتشون یکی نیست
+            if(!isMatching(lastOpener,char)){
+                return false
             }
         }
     }
-    // بعد از بررسی همه‌ی کاراکترها، استک باید خالی باشه
     return stack.isEmpty();
 }
 
-console.log('expression');
-console.log(isBalanced("{[()]}"));        // true
-console.log(isBalanced("({[]})"));        // true
-console.log(isBalanced("({[})"));         // false
+console.log('refactor')
+console.log(isBalanced('(SADf<<sdf>'))
+console.log(isBalanced('(SADf<<sdf>>'))
+console.log(isBalanced('(SADf<<sdf>>)'))
+
