d9af57531bb8a2c16920691f35bf7a0740df9a17
ali <ali.yousefi7518@gmail.com>
2026-08-15 14:35:27 +0430

    implement Stack with array and manual count (push)

--- 03-stack/stack.js
+++ 03-stack/stack.js
@@ -1,56 +1,72 @@
-export default class Stack{
+// export default class Stack{
+//     constructor(){
+//         this.items = [];
+//     }
+
+//     push(element){
+//         this.items.push(element);
+//     }
+
+//     pop(){
+//         if(this.items.length === 0){
+//             return 'stack is empty';
+//         }
+//         return this.items.pop();
+//     }
+
+//     peek(){
+//         if(this.items.length === 0){
+//             return 'stack is empty';
+//         }
+//         return this.items[this.items.length-1];
+//     }
+
+//     isEmpty(){
+//         return this.items.length === 0;
+//     }
+
+//     size(){
+//         return this.items.length;
+//     }
+
+//     print(){
+//         console.log(this.items.join(' -> '));
+//     }
+// }
+
+
+
+// const stack = new Stack();
+
+// console.log(stack.isEmpty()); // true
+
+// stack.push(10);
+// stack.push(20);
+// stack.push(30);
+
+// stack.print(); // 10 -> 20 -> 30
+
+// console.log(stack.peek()); // 30
+// console.log(stack.pop());  // 30
+// console.log(stack.pop());  // 20
+
+// stack.print(); // 10
+
+// console.log(stack.size()); // 1
+// console.log(stack.isEmpty()); // false
+
+
+
+
+// ------------Stack-------------//
+class Stack{
     constructor(){
         this.items = [];
+        this.count = 0;
     }
 
-    push(element){
-        this.items.push(element);
-    }
-
-    pop(){
-        if(this.items.length === 0){
-            return 'stack is empty';
-        }
-        return this.items.pop();
-    }
-
-    peek(){
-        if(this.items.length === 0){
-            return 'stack is empty';
-        }
-        return this.items[this.items.length-1];
-    }
-
-    isEmpty(){
-        return this.items.length === 0;
-    }
-
-    size(){
-        return this.items.length;
-    }
-
-    print(){
-        console.log(this.items.join(' -> '));
+    push(value){
+        this.items[this.count] =value;
+        this.count++
     }
 }
-
-
-
-const stack = new Stack();
-
-console.log(stack.isEmpty()); // true
-
-stack.push(10);
-stack.push(20);
-stack.push(30);
-
-stack.print(); // 10 -> 20 -> 30
-
-console.log(stack.peek()); // 30
-console.log(stack.pop());  // 30
-console.log(stack.pop());  // 20
-
-stack.print(); // 10
-
-console.log(stack.size()); // 1
-console.log(stack.isEmpty()); // false
