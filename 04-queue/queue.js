cf46d850cb62996da56f5f67ec775038c86e69d1
ali <ali.yousefi7518@gmail.com>
2026-08-19 13:58:26 +0430

    implement Queue with manual indexing

--- 04-queue/queue.js
+++ 04-queue/queue.js
@@ -1,42 +1,54 @@
 
-class Queue{
+// class Queue{
+//     constructor(){
+//         this.items =[];
+//     }
+
+//     enqueue(value){
+//         this.items.push(value);// اضافه به انتها
+//     }
+
+//     dequeue(){
+//         if(this.isEmpty()) return 'Queue is empty!';
+//         return this.items.shift()// حذف از ابتدا
+//     }
+
+//     peek(){
+//         if(this.isEmpty()) return 'Queue is empty!';
+//         return this.items[0];
+//     }
+
+//     isEmpty(){
+//         return this.items.length === 0;
+//     }
+
+//     size(){
+//         return this.items.length;
+//     }
+
+//     print(){
+//         console.log(this.items.join(' -> '));
+//     }
+// }
+
+// let queue = new Queue();
+// queue.enqueue(10);
+// queue.enqueue(20);
+// queue.enqueue(30);
+// queue.dequeue();
+// console.log(queue.peek());
+// console.log(queue.isEmpty());
+// console.log(queue.size());
+// queue.print();
+
+
+class Queue {
     constructor(){
-        this.items =[];
+        this.items =[];         // آرایه برای ذخیره عناصر
+        this.count=0;           // تعداد عناصر (و ایندکس بعدی برای اضافه کردن)
+        this.frontIndex =0;     // ایندکس اولین عنصر (برای حذف)
     }
 
-    enqueue(value){
-        this.items.push(value);// اضافه به انتها
-    }
-
-    dequeue(){
-        if(this.isEmpty()) return 'Queue is empty!';
-        return this.items.shift()// حذف از ابتدا
-    }
-
-    peek(){
-        if(this.isEmpty()) return 'Queue is empty!';
-        return this.items[0];
-    }
-
-    isEmpty(){
-        return this.items.length === 0;
-    }
-
-    size(){
-        return this.items.length;
-    }
-
-    print(){
-        console.log(this.items.join(' -> '));
-    }
+    // 1. اضافه کردن به انتهای صف
+    
 }
-
-let queue = new Queue();
-queue.enqueue(10);
-queue.enqueue(20);
-queue.enqueue(30);
-queue.dequeue();
-console.log(queue.peek());
-console.log(queue.isEmpty());
-console.log(queue.size());
-queue.print();
