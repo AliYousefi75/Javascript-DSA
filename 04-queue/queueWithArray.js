0f93096f69451fcf1d6264d3a9f73c7347e0aaeb
ali <ali.yousefi7518@gmail.com>
2026-08-22 12:17:42 +0430

    CircularQueue(add enqueue)

--- 04-queue/queueWithArray.js
+++ 04-queue/queueWithArray.js
@@ -8,4 +8,14 @@ class CircularQueue{
         this.rear = 0;
         this.count = 0;
     }
+
+    enqueue(value){
+        if(this.isFull()){
+            return "Queue is full!";
+        }
+        this.items[this.rear] = value;
+        this.rear = (this.rear + 1) % this.capacity;
+        this.count++;
+
+    }
 }
