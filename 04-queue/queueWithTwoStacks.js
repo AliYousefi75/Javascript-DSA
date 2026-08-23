90d7412d75d6b3b4322bc2a04f503e7fb4a56012
ali <ali.yousefi7518@gmail.com>
2026-08-23 18:42:59 +0430

    implement Queue with two stacks

---
+++ 04-queue/queueWithTwoStacks.js
@@ -0,0 +1,9 @@
+import Stack from "../03-stack/stack.js";
+
+class QueueWithTwoStacks{
+    constructor(){
+        this.stack1 = new Stack();
+        this.stack2 = new Stack();
+        this.size = 0;
+    }
+}
