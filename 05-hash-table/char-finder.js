ca66ef05fe3120d832c2e43e13d7d5a984a2472b
ali <ali.yousefi7518@gmail.com>
2026-09-21 15:02:32 +0430

    Hash-Table(add CharFinder class)

--- 05-hash-table/char-finder.js
+++ 05-hash-table/char-finder.js
@@ -1,20 +1,44 @@
-function findFirstNonRepeatingCharacter(str){
-    const charCount = new Map();
+// function findFirstNonRepeatingCharacter(str){
+//     const charCount = new Map();
 
-    // ۱. شمارش تکرار هر کاراکتر
-    for(let char of str){
-        charCount.set(char, (charCount.get(char) || 0) + 1);
-    }
+//     // ۱. شمارش تکرار هر کاراکتر
+//     for(let char of str){
+//         charCount.set(char, (charCount.get(char) || 0) + 1);
+//     }
 
-    // ۲. پیدا کردن اولین کاراکتر غیرتکراری
-    for (let char of str) {
-        if(charCount.get(char) === 1){
-            return char;
+//     // ۲. پیدا کردن اولین کاراکتر غیرتکراری
+//     for (let char of str) {
+//         if(charCount.get(char) === 1){
+//             return char;
+//         }
+//     }
+
+//     return null;
+// }
+
+// console.log(findFirstNonRepeatingCharacter("aabbcdde"));    // "c"
+// console.log(findFirstNonRepeatingCharacter("leetcode"));    // "l"
+
+
+
+class CharFinder{
+
+
+    findFirstNonRepeatingCharacter(str){
+        let charCount = new Map()
+
+        for(let char of str){
+            charCount.set(char,(charCount.get(char)||0)+1)
         }
+        for(let char of str){
+            if(charCount.get(char) === 1){
+                return char;
+            }
+        }
+        return null;
     }
-
-    return null;
 }
 
-console.log(findFirstNonRepeatingCharacter("aabbcdde"));    // "c"
-console.log(findFirstNonRepeatingCharacter("leetcode"));    // "l"
+
+let finder = new CharFinder();
+console.log(finder.findFirstNonRepeatingCharacter('aavvssg'))
