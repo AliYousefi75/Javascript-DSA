404237ac3cbe40b8bac1e4be613d019da46b5c12
ali <ali.yousefi7518@gmail.com>
2026-09-24 16:34:45 +0330

    Hash-Table(chaining-hash-table add hash-table structure)

--- 05-hash-table/2-chaining-Hash-Table.js
+++ 05-hash-table/2-chaining-Hash-Table.js
@@ -5,3 +5,11 @@ class Node{
         this.next = null;
     }
 }
+
+class HashTable{
+    constructor(size){
+        this.size = size;
+        this.buckets = new Array(size); // هر خونه لیست پیوندیه
+        this.count = 0; // تعداد عناصر
+    }
+}
