dd9db640ca98ea5053a3b4ee6b6821e947d35346
ali <ali.yousefi7518@gmail.com>
2026-09-24 17:32:35 +0330

    Hash-Table(chaining-hash-table add delete method)

--- 05-hash-table/2-chaining-Hash-Table.js
+++ 05-hash-table/2-chaining-Hash-Table.js
@@ -62,4 +62,57 @@ class HashTable{
         }
         return undefined;
     }
+
+    delete(key){
+        const index = this._hash(key);
+        let current = this.buckets[index];
+        let prev = null;
+
+        while(current){
+            if(current.key === key){
+                // اگه اولین گره بود
+                if(prev === null){
+                    this.buckets[index] = current.next;
+                }else{
+                // اگه وسط یا آخر لیست بود    
+                    prev.next = current.next;
+                }
+                this.count--;
+                return true;
+            }
+            prev = current;
+            current = current.next;
+        }
+        return false;// پیدا نشد
+    }
 }
+
+
+
+const ht = new HashTable(10);
+
+// ۱. اضافه کردن
+ht.set("ali", 25);
+ht.set("reza", 30);
+ht.set("sara", 28);
+ht.set("maryam", 22);
+
+console.log(ht.get("ali"));    // 25
+console.log(ht.get("reza"));   // 30
+console.log(ht.get("sara"));   // 28
+
+// ۲. به‌روزرسانی
+ht.set("ali", 26);
+console.log(ht.get("ali"));    // 26
+
+// ۳. حذف
+ht.delete("reza");
+console.log(ht.get("reza"));   // undefined
+
+// ۴. کالیژن (اگه دو تا کلید هش یکسان داشته باشن)
+ht.set("lai", 99);  // "lai" هم هشش با "ali" یکیه (310)
+console.log(ht.get("ali"));    // 26
+console.log(ht.get("lai"));    // 99
+
+// ۵. تعداد
+console.log(ht.count);         // 4
